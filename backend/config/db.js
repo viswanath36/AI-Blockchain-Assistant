6e109d59b3dc99082f5eaa633c02f7f7e3b4b2c9
Viswanath <viswasangaraju@gmail.com>
2026-06-30 20:27:25 +0530

    Remove MongoDB debug logs

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
 
-    console.log("MongoDB Connected");
+    console.log("✅ MongoDB Connected");
   } catch (error) {
     console.error("MongoDB Connection Error:", error.message);
     process.exit(1);
